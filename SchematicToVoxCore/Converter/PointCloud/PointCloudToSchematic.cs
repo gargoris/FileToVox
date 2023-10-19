020a982d091cb7793fd46bb250341f76915ef17c
Francisco Fuentes <Francisco.Fuentes@solera.com>
2023-10-19 12:05:19 +0200

    Update to netcore 7

--- SchematicToVoxCore/Converter/PointCloud/PointCloudToSchematic.cs
+++ SchematicToVoxCore/Converter/PointCloud/PointCloudToSchematic.cs
@@ -8,118 +8,118 @@ using System.Collections.Generic;
 
 namespace FileToVox.Converter.PointCloud
 {
-	public abstract class PointCloudToSchematic : AbstractToSchematic
-	{
-		protected readonly float Scale;
-		protected readonly int ColorLimit;
-		private Schematic mSchematic;
-		protected PointCloudToSchematic(string path, float scale, int colorLimit) : base(path)
-		{
-			Scale = scale;
-			ColorLimit = colorLimit;
-		}
+    public abstract class PointCloudToSchematic : AbstractToSchematic
+    {
+        protected readonly float Scale;
+        protected readonly int ColorLimit;
+        private Schematic mSchematic;
+        protected PointCloudToSchematic(string path, float scale, int colorLimit) : base(path)
+        {
+            Scale = scale;
+            ColorLimit = colorLimit;
+        }
 
-		protected abstract BodyDataDTO ReadContentFile();
+        protected abstract BodyDataDTO ReadContentFile();
 
-		protected void VoxelizeData(BodyDataDTO data)
-		{
-			mSchematic = new Schematic();
+        protected void VoxelizeData(BodyDataDTO data)
+        {
+            mSchematic = new Schematic();
 
-			if (data.BodyVertices.Count == 0)
-			{
-				return;
-			}
+            if (data.BodyVertices.Count == 0)
+            {
+                return;
+            }
 
-			Vector3 minX = data.BodyVertices.MinBy(t => t.X);
-			Vector3 minY = data.BodyVertices.MinBy(t => t.Y);
-			Vector3 minZ = data.BodyVertices.MinBy(t => t.Z);
+            Vector3 minX = data.BodyVertices.MinBy(t => t.X).First();
+            Vector3 minY = data.BodyVertices.MinBy(t => t.Y).First();
+            Vector3 minZ = data.BodyVertices.MinBy(t => t.Z).First();
 
-			float min = Math.Abs(Math.Min(minX.X, Math.Min(minY.Y, minZ.Z)));
+            float min = Math.Abs(Math.Min(minX.X, Math.Min(minY.Y, minZ.Z)));
 
-			for (int i = 0; i < data.BodyVertices.Count; i++)
-			{
-				data.BodyVertices[i] += new Vector3(min, min, min);
-				//data.BodyVertices[i] = new Vector3((float)Math.Truncate(data.BodyVertices[i].X * Scale), (float)Math.Truncate(data.BodyVertices[i].Y * Scale), (float)Math.Truncate(data.BodyVertices[i].Z * Scale));
-			}
+            for (int i = 0; i < data.BodyVertices.Count; i++)
+            {
+                data.BodyVertices[i] += new Vector3(min, min, min);
+                //data.BodyVertices[i] = new Vector3((float)Math.Truncate(data.BodyVertices[i].X * Scale), (float)Math.Truncate(data.BodyVertices[i].Y * Scale), (float)Math.Truncate(data.BodyVertices[i].Z * Scale));
+            }
 
-			Vector3 maxX = data.BodyVertices.MaxBy(t => t.X);
-			Vector3 maxY = data.BodyVertices.MaxBy(t => t.Y);
-			Vector3 maxZ = data.BodyVertices.MaxBy(t => t.Z);
+            Vector3 maxX = data.BodyVertices.MaxBy(t => t.X).First();
+            Vector3 maxY = data.BodyVertices.MaxBy(t => t.Y).First();
+            Vector3 maxZ = data.BodyVertices.MaxBy(t => t.Z).First();
 
-			Console.WriteLine("[INFO] Max X: " + maxX.X + ", Y: " + maxY.Y + ", " + maxZ.Z);
+            Console.WriteLine("[INFO] Max X: " + maxX.X + ", Y: " + maxY.Y + ", " + maxZ.Z);
 
-			minX = data.BodyVertices.MinBy(t => t.X);
-			minY = data.BodyVertices.MinBy(t => t.Y);
-			minZ = data.BodyVertices.MinBy(t => t.Z);
+            minX = data.BodyVertices.MinBy(t => t.X).First();
+            minY = data.BodyVertices.MinBy(t => t.Y).First();
+            minZ = data.BodyVertices.MinBy(t => t.Z).First();
 
-			Console.WriteLine("[INFO] Min X: " + minX.X + ", Y: " + minY.Y + ", " + minZ.Z);
+            Console.WriteLine("[INFO] Min X: " + minX.X + ", Y: " + minY.Y + ", " + minZ.Z);
 
-			Vector3 size = new Vector3(maxX.X - minX.X, maxY.Y - minY.Y, maxZ.Z - minZ.Z);
+            Vector3 size = new Vector3(maxX.X - minX.X, maxY.Y - minY.Y, maxZ.Z - minZ.Z);
 
-			Console.WriteLine("[INFO] Size X: " + size.X + ", Y: " + size.Y + ", " + size.Z);
+            Console.WriteLine("[INFO] Size X: " + size.X + ", Y: " + size.Y + ", " + size.Z);
 
-			float max = Math.Max(size.X, Math.Max(size.Y, size.Z));
-			float factor = Scale / max;
+            float max = Math.Max(size.X, Math.Max(size.Y, size.Z));
+            float factor = Scale / max;
 
-			for (int i = 0; i < data.BodyVertices.Count; i++)
-			{
-				data.BodyVertices[i] = new Vector3((float)Math.Truncate(data.BodyVertices[i].X * factor), (float)Math.Truncate(data.BodyVertices[i].Y * factor), (float)Math.Truncate(data.BodyVertices[i].Z * factor));
-			}
+            for (int i = 0; i < data.BodyVertices.Count; i++)
+            {
+                data.BodyVertices[i] = new Vector3((float)Math.Truncate(data.BodyVertices[i].X * factor), (float)Math.Truncate(data.BodyVertices[i].Y * factor), (float)Math.Truncate(data.BodyVertices[i].Z * factor));
+            }
 
-			minX = data.BodyVertices.MinBy(t => t.X);
-			minY = data.BodyVertices.MinBy(t => t.Y);
-			minZ = data.BodyVertices.MinBy(t => t.Z);
+            minX = data.BodyVertices.MinBy(t => t.X).First();
+            minY = data.BodyVertices.MinBy(t => t.Y).First();
+            minZ = data.BodyVertices.MinBy(t => t.Z).First();
 
-			//HashSet<Vector3> set = new HashSet<Vector3>();
+            //HashSet<Vector3> set = new HashSet<Vector3>();
 
-			min = Math.Min(minX.X, Math.Min(minY.Y, minZ.Z));
+            min = Math.Min(minX.X, Math.Min(minY.Y, minZ.Z));
 
-			Console.WriteLine("[INFO] Started to voxelize data...");
-			using (ProgressBar progressbar = new ProgressBar())
-			{
-				for (int i = 0; i < data.BodyVertices.Count; i++)
-				{
-					float maxV = Math.Max(data.BodyVertices[i].X, Math.Max(data.BodyVertices[i].Y, data.BodyVertices[i].Z));
-					if (maxV - min >= 0)
-					{
-						data.BodyVertices[i] -= new Vector3(min, min, min);
-						mSchematic.AddVoxel((int)(data.BodyVertices[i].X - minX.X), (int)(data.BodyVertices[i].Y - minY.Y), (int)(data.BodyVertices[i].Z - minZ.Z), data.BodyColors[i].ColorToUInt());
-					}
-					//if (!set.Contains(data.BodyVertices[i]))
-					//{
-					//	set.Add(data.BodyVertices[i]);
-					//	//vertices.Add(data.BodyVertices[i]);
-					//	//colors.Add(data.BodyColors[i]);
-					//}
-					progressbar.Report(i / (float)data.BodyVertices.Count);
-				}
-			}
+            Console.WriteLine("[INFO] Started to voxelize data...");
+            using (ProgressBar progressbar = new ProgressBar())
+            {
+                for (int i = 0; i < data.BodyVertices.Count; i++)
+                {
+                    float maxV = Math.Max(data.BodyVertices[i].X, Math.Max(data.BodyVertices[i].Y, data.BodyVertices[i].Z));
+                    if (maxV - min >= 0)
+                    {
+                        data.BodyVertices[i] -= new Vector3(min, min, min);
+                        mSchematic.AddVoxel((int)(data.BodyVertices[i].X - minX.X), (int)(data.BodyVertices[i].Y - minY.Y), (int)(data.BodyVertices[i].Z - minZ.Z), data.BodyColors[i].ColorToUInt());
+                    }
+                    //if (!set.Contains(data.BodyVertices[i]))
+                    //{
+                    //	set.Add(data.BodyVertices[i]);
+                    //	//vertices.Add(data.BodyVertices[i]);
+                    //	//colors.Add(data.BodyColors[i]);
+                    //}
+                    progressbar.Report(i / (float)data.BodyVertices.Count);
+                }
+            }
 
-			Console.WriteLine("[INFO] Done.");
+            Console.WriteLine("[INFO] Done.");
 
-			//minX = vertices.MinBy(t => t.X);
-			//minY = vertices.MinBy(t => t.Y);
-			//minZ = vertices.MinBy(t => t.Z);
+            //minX = vertices.MinBy(t => t.X);
+            //minY = vertices.MinBy(t => t.Y);
+            //minZ = vertices.MinBy(t => t.Z);
 
-			//for (int i = 0; i < vertices.Count; i++)
-			//{
-			//	float max = Math.Max(vertices[i].X, Math.Max(vertices[i].Y, vertices[i].Z));
-			//	if (/*max - min < 8000 && */max - min >= 0)
-			//	{
-			//		vertices[i] -= new Vector3(min, min, min);
-			//		_blocks.Add(new Voxel((ushort)vertices[i].X, (ushort)vertices[i].Y, (ushort)vertices[i].Z, colors[i].ColorToUInt()));
-			//	}
-			//}
-		}
-
-
-		public override Schematic WriteSchematic()
-		{
-			List<Voxel> list = Quantization.ApplyQuantization(mSchematic.GetAllVoxels(), ColorLimit);
-			Schematic schematic = new Schematic(list);
-			return schematic;
-		}
+            //for (int i = 0; i < vertices.Count; i++)
+            //{
+            //	float max = Math.Max(vertices[i].X, Math.Max(vertices[i].Y, vertices[i].Z));
+            //	if (/*max - min < 8000 && */max - min >= 0)
+            //	{
+            //		vertices[i] -= new Vector3(min, min, min);
+            //		_blocks.Add(new Voxel((ushort)vertices[i].X, (ushort)vertices[i].Y, (ushort)vertices[i].Z, colors[i].ColorToUInt()));
+            //	}
+            //}
+        }
 
 
-	}
+        public override Schematic WriteSchematic()
+        {
+            List<Voxel> list = Quantization.ApplyQuantization(mSchematic.GetAllVoxels(), ColorLimit);
+            Schematic schematic = new Schematic(list);
+            return schematic;
+        }
+
+
+    }
 }
--- SchematicToVoxCore/Converter/VoxToSchematic.cs
+++ SchematicToVoxCore/Converter/VoxToSchematic.cs
@@ -1,62 +1,61 @@
-﻿using FileToVoxCore.Extensions;
+﻿using FileToVox.Extensions;
+using FileToVoxCore.Extensions;
 using FileToVoxCore.Schematics;
 using FileToVoxCore.Schematics.Tools;
 using FileToVoxCore.Utils;
 using FileToVoxCore.Vox;
 using MoreLinq;
-using System.Drawing;
-using FileToVox.Extensions;
 
 namespace FileToVox.Converter
 {
-	public class VoxToSchematic : AbstractToSchematic
-	{
-		private VoxModel mVoxModel;
-		public VoxToSchematic(string path) : base(path)
-		{
-			VoxReader reader = new VoxReader();
-			mVoxModel = reader.LoadModel(path);
-		}
+    public class VoxToSchematic : AbstractToSchematic
+    {
+        private VoxModel mVoxModel;
+        public VoxToSchematic(string path) : base(path)
+        {
+            VoxReader reader = new VoxReader();
+            mVoxModel = reader.LoadModel(path);
+        }
 
-		public override Schematic WriteSchematic()
-		{
-			Schematic schematic = new Schematic();
-			FileToVoxCore.Drawing.Color[] colorsPalette = mVoxModel.Palette;
-			using (ProgressBar progressbar = new ProgressBar())
-			{
-				int minX = (int) mVoxModel.TransformNodeChunks.MinBy(t => t.TranslationAt().X).TranslationAt().X;
-				int minY = (int) mVoxModel.TransformNodeChunks.MinBy(t => t.TranslationAt().Y).TranslationAt().Y;
-				int minZ = (int) mVoxModel.TransformNodeChunks.MinBy(t => t.TranslationAt().Z).TranslationAt().Z;
+        public override Schematic WriteSchematic()
+        {
+            Schematic schematic = new Schematic();
+            FileToVoxCore.Drawing.Color[] colorsPalette = mVoxModel.Palette;
+            using (ProgressBar progressbar = new ProgressBar())
+            {
+                int minX = (int)mVoxModel.TransformNodeChunks.MinBy(t => t.TranslationAt().X).First().TranslationAt().X;
+                int minY = (int)mVoxModel.TransformNodeChunks.MinBy(t => t.TranslationAt().Y).First().TranslationAt().Y;
+                int minZ = (int)mVoxModel.TransformNodeChunks.MinBy(t => t.TranslationAt().Z).First().TranslationAt().Z;
 
-				for (int i = 0; i < mVoxModel.VoxelFrames.Count; i++)
-				{
-					VoxelData data = mVoxModel.VoxelFrames[i];
-					Vector3 worldPositionFrame = mVoxModel.TransformNodeChunks[i + 1].TranslationAt();
+                for (int i = 0; i < mVoxModel.VoxelFrames.Count; i++)
+                {
+                    VoxelData data = mVoxModel.VoxelFrames[i];
+                    Vector3 worldPositionFrame = mVoxModel.TransformNodeChunks[i + 1].TranslationAt();
 
-					if (worldPositionFrame == Vector3.zero)
-						continue;
+                    if (worldPositionFrame == Vector3.zero)
+                        continue;
 
-					for (int y = 0; y < data.VoxelsTall; y++)
-					{
-						for (int z = 0; z < data.VoxelsDeep; z++)
-						{
-							for (int x = 0; x < data.VoxelsWide; x++)
-							{
-								int indexColor = data.Get(x, y, z);
-								FileToVoxCore.Drawing.Color color = colorsPalette[indexColor];
-								if (color != FileToVoxCore.Drawing.Color.Empty)
-								{
-									schematic.AddVoxel((int)(z + worldPositionFrame.X - minX), (int)(y + (worldPositionFrame.Z - minZ)), (int)(x + worldPositionFrame.Y - minY), color.ColorToUInt());
-								}
-							}
-						}
-					}
-					progressbar.Report(i / (float)mVoxModel.VoxelFrames.Count);
-				}
-			}
+                    for (int y = 0; y < data.VoxelsTall; y++)
+                    {
+                        for (int z = 0; z < data.VoxelsDeep; z++)
+                        {
+                            for (int x = 0; x < data.VoxelsWide; x++)
+                            {
+                                int indexColor = data.Get(x, y, z);
+                                FileToVoxCore.Drawing.Color color = colorsPalette[indexColor];
+                                if (color != FileToVoxCore.Drawing.Color.Empty)
+                                {
+                                    schematic.AddVoxel((int)(z + worldPositionFrame.X - minX), (int)(y + (worldPositionFrame.Z - minZ)), (int)(x + worldPositionFrame.Y - minY), color.ColorToUInt());
+                                }
+                            }
+                        }
+                    }
+                    progressbar.Report(i / (float)mVoxModel.VoxelFrames.Count);
+                }
+            }
 
 
-			return schematic;
-		}
-	}
+            return schematic;
+        }
+    }
 }
